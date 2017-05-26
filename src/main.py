4398fd27936b04f771e3075ab211116478e007f6
Erolon <potionist1@gmail.com>
2017-05-26 16:30:17 +0300

    Added a basic UI

--- src/main.py
+++ src/main.py
@@ -8,14 +8,13 @@ from game_map import Game_Map
 screen_width = 30
 screen_height = 50
 LIMIT_FPS = 30
-current_level = 1
 
 def handle_keys(player): 
     user_input = tdl.event.key_wait()
     # Movement keys
     if user_input.key == 'UP':
         newY = player.y - 1
-        if not game_map.map_list[newY][player.x].blocked and (player.x, newY) in con:
+        if not game_map.map_list[newY][player.x].blocked and (player.x, newY) in con: #TODO FIX
             player.move(0, -1)
     elif user_input.key == 'DOWN':
         newY = player.y + 1
@@ -44,11 +43,10 @@ def tileForChar(char):
 
 player = None # Initialized in loadMap
 
-def loadMap():
-    global current_level
+def loadMap(current_level):
     global player
 
-    with open("level" + str(current_level) + ".map") as file:
+    with open("Maps/level" + str(current_level) + ".map") as file:
         mapList =  [[tileForChar(char) for char in list(line.rstrip())] for line in file]
     height = len(mapList) # Always reference mapList [y][x]!
     width = len(mapList[0])
@@ -83,20 +81,50 @@ def render_all(): ## Needs to be modified later
 
     root.blit(con, 0, 0, screen_width, screen_height, 0, 0) # move the console's contents to the root console
 
+    # Prepare to render the UI Panel
+    panel.clear(fg=(255, 255, 255), bg=(0, 0, 0))
+ 
+    # Render the bars here
+    render_bar(1, 1, BAR_WIDTH, 'HP', 10, 15, (200, 0, 0), (160, 0, 0)) # HP BAR
+ 
+    # Move panel contents to the root panel
+    root.blit(panel, 0, PANEL_Y, screen_width, PANEL_HEIGHT, 0, 0)
+
+def render_bar(x, y, total_width, name, value, maximum, bar_color, back_color):
+    # Calculate the width of the bar
+    bar_width = int(float(value) / maximum * total_width)
+ 
+    # Draw the background
+    panel.draw_rect(x, y, total_width, 1, None, bg=back_color)
+ 
+    # Draw the bar on top
+    if bar_width > 0:
+        panel.draw_rect(x, y, bar_width, 1, None, bg=bar_color)
+
+    # Draw the actual values onto the bar
+    text = name + ': ' + str(value) + '/' + str(maximum)
+    x_centered = x + (total_width-len(text)) // 2
+    panel.draw_str(x_centered, y, text, fg=(0, 0, 0), bg=None)
+
 tdl.set_font('Assets/terminal8x8_gs_ro.png')
 
-game_map = loadMap()
+current_level = 1 # change later
+game_map = loadMap(current_level)
 screen_height = game_map.height
 screen_width = game_map.width
+
+BAR_WIDTH = 20
+PANEL_HEIGHT = 7
+PANEL_Y = screen_height - PANEL_HEIGHT
 
 root = tdl.init(screen_width, screen_height, title="Game", fullscreen=False)
 tdl.setFPS(LIMIT_FPS)
 con = tdl.Console(screen_width, screen_height)
+panel = tdl.Console(screen_width, PANEL_HEIGHT)
 
 # player = Object(screen_width // 2, screen_height // 2, '@', (255,255,255)) # // = integer division
 npc = Object(screen_width // 2 - 5, screen_height // 2, '@', (255,255,0))
 objects = [npc, player]
-
 
 while not tdl.event.is_window_closed():
 
